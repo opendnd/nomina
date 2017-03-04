a21d59d9f65a6f6c6c956c0c9328c3dccde39e0d
Drew Morris <dhmorris@gmail.com>
2017-03-04 18:13:42 -0500

    Adding the project files for easily generating names

---
+++ test/lib/nomina-test.js
@@ -0,0 +1,49 @@
+const expect = require('chai').expect;
+const fs = require('fs');
+const path = require('path');
+const rootDir = path.join(__dirname, '..', '..');
+const libDir = path.join(rootDir, 'lib');
+const binDir = path.join(rootDir, 'bin');
+const spawn = require('child_process').spawn;
+let nomina;
+let output = '';
+
+describe('nomina', () => {
+  describe('bin', () => {
+    before((done) => {
+      const options = [
+        '--type', 'medieval', 
+        '--gender', 'female'
+      ];
+      nomina = spawn(path.join(binDir, 'nomina'), options);
+
+      nomina.stdout.on('data', (data) => {
+        output += data;
+      });
+
+      nomina.on('close', () => {
+        done();
+      });
+    });
+
+    it('outputs something', () => {
+      expect(output.length).to.be.above(1);
+    });
+  });
+
+  describe('module', () => {
+    before(() => {
+      nomina = require(path.join(libDir, 'nomina'));
+    });
+
+    it('outputs a name', () => {
+      const options = {
+        type: 'medieval',
+        gender: 'female'
+      };
+
+      const result = nomina(options);
+      expect(result.length).to.be.above(1);
+    });
+  });
+});
